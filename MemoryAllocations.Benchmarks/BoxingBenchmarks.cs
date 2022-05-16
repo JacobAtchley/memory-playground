fb93bcc8e429509a0d2c57e2abce6520253d95b2
Jacob Atchley <yaaqob.atchley@gmail.com>
2022-05-16 18:11:37 -0500

    boxing

---
+++ MemoryAllocations.Benchmarks/BoxingBenchmarks.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
+using BenchmarkDotNet.Attributes;
+
+namespace MemoryAllocations.Benchmarks;
+
+[MemoryDiagnoser]
+[SuppressMessage("Performance", "CA1822:Mark members as static")]
+public class BoxingBenchmarks
+{
+   [Benchmark]
+   public void Boxed()
+   {
+      object first = 1;
+      var second = 2;
+      
+      var result = (int)first + second;
+   }
+   [Benchmark]
+   public void UnBoxed()
+   {
+      var first = 1;
+      var second = 2;
+      
+      var result = first + second;
+   }
+}
